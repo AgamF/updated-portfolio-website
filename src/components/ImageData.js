7019b425efc3aa1cbaaa8747bd9f05dec5c610d9
AgamF <93123804+AgamF@users.noreply.github.com>
2022-05-17 18:09:37 +0300

    commit 22

--- src/components/ImageData.js
+++ src/components/ImageData.js
@@ -1,6 +1,7 @@
 import githubImage from '../images/github.png';
 import instagramImage from '../images/insta.png';
 import youtubeImage from '../images/youtube.png';
+import linkedinimage from '../images/icons8-linkedin-48.png';
 
 export const imageData = [
   {
@@ -23,5 +24,12 @@ export const imageData = [
     link: 'https://www.youtube.com/channel/UCwXrcNE36pme9H1nu9GOdMw',
     dimensions: '60px',
     key: '3'
+  },
+  {
+    title: 'LinkedIn',
+    src: linkedinimage,
+    link: 'https://www.linkedin.com/in/agam-farajpur-64096423a/',
+    dimensions: '60px',
+    key: '4'
   }
 ]
