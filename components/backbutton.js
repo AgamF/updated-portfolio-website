8e75580fc0ae5bfc286ca769316b9cd32af1a005
AgamF <93123804+AgamF@users.noreply.github.com>
2022-11-02 10:59:29 +0200

    Delete components directory

--- components/backbutton.js
+++
@@ -1,7 +0,0 @@
-export default function BackButton() {
-  return (
-    <button id="back-button" onClick={() => window.history.back()}>
-       {".".replace(".", "<<")} Previous Page
-    </button>
-  );
-}
--- components/header.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function Header(props) {
-  return (
-    <Head>
-      <title>Agam Farajpur | {props.title}</title>
-    </Head>
-  );
-}
