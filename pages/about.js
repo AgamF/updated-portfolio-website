11334678c9f4ecf5fa69b68aad4a2e5990a43594
AgamF <93123804+AgamF@users.noreply.github.com>
2025-11-02 20:07:37 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -20,16 +20,11 @@ export default function AboutPage() {
       <div id="about-wrapper">
         <img src="me.jpg"/>
         <h1 style={{fontWeight: "200"}}>Yep, that's me.</h1>
-        <p style={uniqueParagraphStyle}>Hi! My name is Agam and i'm an 18 year old web-developer and software engineer based in Israel. <br/>
-        I learned Python programming about 3 years ago and web-development with HTML, CSS & Javascript about 2 years ago. <br/>
-        Ever since then i proceeded to enrich my knowledge even more by learning a vast amount of frameworks, technologies, <br/>
-        programming languages, concepts, methodologies and so much more.<br/><br/>
-
-        I'm a High-school graduate aswell as a CS50 edx graduate and a ZeroToMastery academy graduate.<br/>
-        As of 25th of January, 2022, I will serve in the military as a Naval warrior as part of a mandatory 3 year service.<br/><br/>
-
-        As of october 2022, I became proficient at 7 programming languages & have over 12 maintained projects on Github, <br/> and I came to an understanding that i love programming and that i am very passionate <br/>
-        about problem-solving. Gym is essential, Chess is for life ♥. <br/><br/>
+        <p style={uniqueParagraphStyle}>Hi, I’m Agam Farajpur, an 18-year-old Web Developer and Junior Software Engineer based in Israel.
+            I have a strong foundation in Python and full-stack web development using HTML, CSS, and JavaScript, with experience across multiple frameworks and technologies.
+            Over the past few years, I’ve built and maintained numerous projects on GitHub, focusing on clean code, scalability, and performance.
+            My technical background also includes exposure to DevOps practices, automation, and cloud deployment, which I’m actively expanding toward a professional DevOps career.
+            I’m a CS50 (Harvard/edX) and Zero to Mastery Academy graduate, passionate about continuous learning, problem-solving, and building practical, real-world solutions through code.
         </p>
         <form method="get" action="Agam Farajpur CV.pdf">
           <button className="menu-btn" type="submit">My Resume</button>
@@ -38,4 +33,5 @@ export default function AboutPage() {
       <ParticlesComponent/>
     </Fragment>
   );
+
 }
