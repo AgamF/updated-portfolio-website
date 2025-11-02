4218783ceba64616b083fde8d423153ad131896d
AgamF <93123804+AgamF@users.noreply.github.com>
2025-11-02 20:11:08 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -20,7 +20,7 @@ export default function AboutPage() {
       <div id="about-wrapper">
         <img src="me.jpg"/>
         <h1 style={{fontWeight: "200"}}>Yep, that's me.</h1>
-        <p style={uniqueParagraphStyle}>Hi, I’m Agam Farajpur, an 18-year-old Web Developer and Junior Software Engineer based in Israel.
+        <p style={uniqueParagraphStyle}>Hi, I’m Agam Farajpur, a 21 year-old Web Developer and Junior Software Engineer based in Israel.
             I have a strong foundation in Python and full-stack web development using HTML, CSS, and JavaScript, with experience across multiple frameworks and technologies.
             Over the past few years, I’ve built and maintained numerous projects on GitHub, focusing on clean code, scalability, and performance.
             My technical background also includes exposure to DevOps practices, automation, and cloud deployment, which I’m actively expanding toward a professional DevOps career.
@@ -35,3 +35,4 @@ export default function AboutPage() {
   );
 
 }
+
